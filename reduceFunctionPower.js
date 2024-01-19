cce70b17d65a4ef5f61fb04843848557f66cd339
adibahbab4108 <adibalvi476@gmail.com>
2024-01-20 00:57:45 +0600

    Power of reduce functions exposed

--- reduceFunctionPower.js
+++ reduceFunctionPower.js
@@ -1,5 +1,8 @@
 const numbers = [1, 2, 3, 4, false, "", NaN, 5, 6];
-// return string sum
+const names = [
+    "Adib", "Ahbab", "Rakib", "Shafiq", "Zabbar", "Ifty", "Imrus", "Nahiyan", "Ibrahim", "Rayan", "Ayan"
+]
+// return string sum.....................................
 const result1 = numbers.reduce((sum, currentValue, index) => {
     if (index === 0) sum += '[';
 
@@ -11,10 +14,30 @@ const result1 = numbers.reduce((sum, currentValue, index) => {
     return sum;
 }, '')// sum er initial value '' diye disi zeheto string return korbo(ja return korbo ekane tai dibo)
 
-console.log(result1);
+// console.log(result1);
 
-// return array sum
+// return array sum............................
 const result2 = numbers.reduce((sum, currentValue) => {
     if (currentValue) sum.push(currentValue.toString())
     return sum;
 }, [])// eibare array return korbo tai [] disi
+
+//grouping the names with first letter............
+const groupedNames = names.reduce((acc, cur) => {
+    const firstLetter = cur[0].toUpperCase();
+
+    // Check if the firstLetter key exists in the accumulator
+    if (acc[firstLetter]) {
+        acc[firstLetter].push(cur);
+    } else {
+        // If not, create a new array with the current name
+        acc[firstLetter] = [cur];
+    }
+    return acc;
+}, {});
+// console.log(groupedNames);
+
+Object.keys(groupedNames).forEach(groupKey => {
+    console.log('_____', groupKey, '_____');
+    groupedNames[groupKey].forEach(name => console.log(name));
+})
